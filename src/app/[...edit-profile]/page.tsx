4c2d0d5e52abff3ae5a274b8275e4d9b8b081392
flizze <alexandre.bel@hotmail.com>
2024-10-10 20:23:37 +0000

    edited a fetch that was still on localhost

--- src/app/[...edit-profile]/page.tsx
+++ src/app/[...edit-profile]/page.tsx
@@ -93,7 +93,7 @@ export default function EditProfile() {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/api/users", {
+      const response = await fetch("https://api.calendar.alexandrebel.me/users", {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
